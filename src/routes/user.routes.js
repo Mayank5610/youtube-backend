408a593c6bb6b1cbda437c877f4465b8212f2923
Mayank5610 <mayankpatel5610@gmail.com>
2025-01-19 13:51:22 +0530

    -Added register model and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
 const { registerUser } = require("../controllers/user.controllers");
+const upload = require("../middlewares/multer.middlewares");
 
 const userRouter = Router();
 
-userRouter.route("/register").post(registerUser);
+userRouter.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 module.exports = { userRouter };
